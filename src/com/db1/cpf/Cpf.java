c6555285060309376928ce81f233a179cfebdbd3
Alexandremma <46142888+Alexandremma@users.noreply.github.com>
2019-09-13 17:14:47 -0300

    Realizada a refatoração do código

--- src/com/db1/cpf/Cpf.java
+++ src/com/db1/cpf/Cpf.java
@@ -1,36 +1,22 @@
 package com.db1.cpf;
 
+import java.util.Objects;
+
 public class Cpf {
-	// atributos
+	//atributos
+	private static final String[] CPFS_INVALIDOS = {"00000000000", "11111111111", "22222222222", "33333333333", "44444444444", "55555555555", "66666666666", "77777777777", "88888888888", "99999999999"};
+	
 	private String cpf;
 	
 	//construtor
 	public Cpf(String cpf) {	
-		cpf = cpf.replace(" ", ""); //tira espaços em branco
-		cpf = cpf.replace(".", ""); //tira pontos
-		cpf = cpf.replace("-", ""); //tira traços
+		Objects.requireNonNull(cpf, "Valor cpf é obrigatório");
 		
-		//verificar se CPF válido
-		if (verificaCpf(cpf)) {
-			this.cpf = cpf;
-			System.out.println("CPF inserido");
-		} else {
-			System.out.println("CPF inválido");
-		}
-	}
-	
-	//métodos de get e set
-	public void setCpf(String cpf) {
-		cpf = cpf.replaceAll(" ", ""); //tira espaços em branco
-		cpf = cpf.replaceAll(".", ""); //tira pontos
-		cpf = cpf.replaceAll("-", ""); //tira traços
+		this.cpf = cpf.replace(" ", "").replace(".", "").replace("-", ""); //tira espaços em branco, pontos e traços
 		
-		if (verificaCpf(cpf)) {
-			this.cpf = cpf;
-			System.out.println("CPF inserido");
-		} else {
-			System.out.println("CPF inválido");
-		}
+		verificaCpf();
+		
+		System.out.println("CPF inserido");
 	}
 	
 	public String getCpfComMascara() {
@@ -53,83 +39,46 @@ public class Cpf {
 	}
 	
 	//métodos
-	public boolean verificaCpf(String cpf) {
+	private void verificaCpf() {
 		
-//		if (cpf == "00000000000" || 
-//			cpf == "11111111111" || 
-//			cpf == "22222222222" || 
-//			cpf == "33333333333" || 
-//			cpf == "44444444444" || 
-//			cpf == "55555555555" || 
-//			cpf == "66666666666" || 
-//			cpf == "77777777777" || 
-//			cpf == "88888888888" || 
-//			cpf == "99999999999" ||
-//			cpf.length() != 11 ) {
-		if (cpf.equals("00000000000") ||
-	        cpf.equals("11111111111") ||
-	        cpf.equals("22222222222") || cpf.equals("33333333333") ||
-	        cpf.equals("44444444444") || cpf.equals("55555555555") ||
-	        cpf.equals("66666666666") || cpf.equals("77777777777") ||
-	        cpf.equals("88888888888") || cpf.equals("99999999999") ||
-	        (cpf.length() != 11)) {
-			System.out.println("Cpf com numeros iguais ou não tem 11 digitos");
-			return false;
+		verificaNumeroDeDigitosDoCpf();
+		
+		verificaSeEstaEmCpfsInvalidos();
+		
+		int verificador1 = calculaDigitoVerificadorDoCpf(10);
+		
+		verificaSePrimeiroDigitoVerificadorEstaCorreto(verificador1);
+		
+		int verificador2 = calculaDigitoVerificadorDoCpf(11);
+		
+		verificaSeSegundoDigitoVerificadorEstaCorreto(verificador2);
+		
+	}
+	
+	private void verificaNumeroDeDigitosDoCpf() {
+		if (cpf.length() != 11) {
+			throw new RuntimeException("Cpf deve ter 11 dígitos");
 		}
-		
-		// --------- analisar numeros verificadores
-		// primeiro número
-		String subCpf = "";
-		int a = 0;
-		int b = 1;
+	}
+	
+	private void verificaSeEstaEmCpfsInvalidos() {
+		for (String cpfInvalido : CPFS_INVALIDOS) {
+			if (cpf.equals(cpfInvalido)) {
+				throw new RuntimeException("Cpf com numeros iguais");
+			}
+		}
+	}
+	
+	private int calculaDigitoVerificadorDoCpf(int peso) {
 		int numeroCpf;
-		int peso = 10;
 		int multi;
 		int soma = 0;
+		int a = 0;
+		int b = 1;
+		int limiteDoLoop = peso - 1;
 		
-		for (int i = 0; i < 9; i++) {
-			subCpf = cpf.substring(a, b);
-			
-			numeroCpf = Integer.parseInt(subCpf);
-			
-			multi = (peso * numeroCpf);
-			
-			soma += multi;
-			
-			a++;
-			b++;
-			peso--;
-		}
-		
-		int resultado;
-		
-		resultado = soma % 11;
-		
-		int verificador = 11 - resultado;
-		
-		if (verificador > 9) {
-			verificador = 0;
-		}
-		
-		int cpfComparador;
-		
-		cpfComparador = Integer.parseInt(cpf.substring(9, 10)); 
-		
-		if (verificador != cpfComparador) {
-			System.out.println("Primeiro verificador errado");
-			return false;
-		}
-		
-		//segundo número
-		a = 0;
-		b = 1;
-		peso = 11;
-		soma = 0;
-		
-		for (int i = 0; i < 10; i++) {
-			subCpf = cpf.substring(a, b);
-			
-			numeroCpf = Integer.parseInt(subCpf);
+		for (int i = 0; i < limiteDoLoop; i++) {
+			numeroCpf = Integer.parseInt(cpf.substring(a, b));
 			
 			multi = (peso * numeroCpf);
 			
@@ -140,21 +89,28 @@ public class Cpf {
 			peso--;
 		}
 		
-		resultado = soma % 11;
-		
-		verificador = 11 - resultado;
+		int verificador = 11 - (soma % 11);
 		
 		if (verificador > 9) {
 			verificador = 0;
 		}
 		
-		cpfComparador = Integer.parseInt(cpf.substring(10, 11)); 
+		return verificador;
+	}
+	
+	private void verificaSePrimeiroDigitoVerificadorEstaCorreto(int verificador) {
+		int cpfComparador = Integer.parseInt(cpf.substring(9, 10)); 
 		
 		if (verificador != cpfComparador) {
-			System.out.println("Segundo verificador errado");
-			return false;
+			throw new RuntimeException("Primeiro verificador errado");
 		}
+	}
+	
+	private void verificaSeSegundoDigitoVerificadorEstaCorreto(int verificador) {
+		int cpfComparador = Integer.parseInt(cpf.substring(10, 11)); 
 		
-		return true;
+		if (verificador != cpfComparador) {
+			throw new RuntimeException("Segundo verificador errado");
+		}
 	}
 }
